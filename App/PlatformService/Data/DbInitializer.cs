a12c699d32f34c51d227853a1f35b233fda13b99
maurnl <mauronluciano@hotmail.com>
2023-01-12 14:21:39 -0300

    added production database

--- App/PlatformService/Data/DbInitializer.cs
+++ App/PlatformService/Data/DbInitializer.cs
@@ -1,26 +1,33 @@
-﻿using PlatformService.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public static class DbInitializer
     {
-        public static void Populate(IApplicationBuilder app)
+        public static void Populate(IApplicationBuilder app, bool isProduction)
         {
             using(var scope = app.ApplicationServices.CreateScope())
             {
-                SeedData(scope.ServiceProvider.GetRequiredService<AppDbContext>());
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                SeedData(dbContext);
+                if(isProduction)
+                {
+                    dbContext.Database.Migrate();
+
+                }
             }
         }
 
         private static void SeedData(AppDbContext context)
         {
-            if(!context.Platforms.Any())
+            if (!context.Platforms.Any())
             {
                 Console.WriteLine("Seeding data");
                 context.Platforms.AddRange(
-                    new Platform { Name = "Dot Net", Publisher = "Microsoft", Cost="Free" },
+                    new Platform { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
                     new Platform { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
-                    new Platform { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" } 
+                    new Platform { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
                 );
 
                 context.SaveChanges();
--- App/PlatformService/Program.cs
+++ App/PlatformService/Program.cs
@@ -1,13 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
 using PlatformService.SyncDataServices.Http;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseInMemoryDatabase("inmem_database");
+    if (builder.Environment.IsDevelopment())
+    {
+        Console.WriteLine("=> Using in memory database");
+        options.UseInMemoryDatabase("inmem_database");
+    }
+    else
+    {
+        Console.WriteLine("=> Using mssql database");
+        options.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConnectionString"));
+    }
 });
 builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
@@ -30,6 +40,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-DbInitializer.Populate(app);
+DbInitializer.Populate(app, builder.Environment.IsProduction());
 
 app.Run();
